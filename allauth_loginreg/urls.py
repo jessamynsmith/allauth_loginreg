fcdedec87f484d31925189fed55d2a3f8c9f343a
Alicia Alexander <aliciaalexander@Alicias-MacBook-Pro.local>
2017-12-09 15:06:24 -0500

    Updates

--- allauth_loginreg/urls.py
+++ allauth_loginreg/urls.py
@@ -20,5 +20,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('allauth.urls')),
-    url(r'^$', RedirectView.as_view(url='accounts/signup/', permanent=False)),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^$', RedirectView.as_view(url='accounts/login/', permanent=False)),
 ]
